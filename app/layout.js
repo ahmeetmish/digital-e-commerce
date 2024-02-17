4d03fd4142fe727a732580e68eb7513cd85c0b40
ahmet <codingwithmish@gmail.com>
2024-02-17 17:52:43 +0300

    components integrated

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import "./globals.css"
 import { Poppins } from "next/font/google"
 
+import Navbar from "./_components/Navbar"
+import Footer from "./_components/Footer"
+
 const poppins = Poppins({ subsets: ["latin"], weight: ['200', '300', '400', '500', '600', '700'] })
 
 export const metadata = {
@@ -10,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
     </html>
   )
 }
