d10a1865e4061da98ed1a0840c2e8756893e2ede
ahmet <codingwithmish@gmail.com>
2024-02-19 01:43:37 +0300

    fix image height disproportionality

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -5,7 +5,7 @@ export default function ProductItem({ product }) {
   return (
     <Link href={`/product/${product.id}`}>
     <div className="min-h-[281px] border-2 hover:border-violet-300 rounded-md cursor-pointer transition-all">
-      <Image src={product.banner} alt={product.name} width={300} height={240} className="overflow-hidden" />
+      <Image src={product.banner} alt={product.name} width={300} height={240} className="overflow-hidden h-[230px]" />
       <div className="p-2 flex items-center justify-between">
         <div>
           <h2 className="text-base text-neutral-600 mb-[-4px]">{product.name}</h2>
