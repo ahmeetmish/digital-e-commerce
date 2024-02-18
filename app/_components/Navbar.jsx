a9bbfdab037270b5eb141392dbc9d55decc2a296
ahmet <codingwithmish@gmail.com>
2024-02-18 18:24:09 +0300

    Navbar, Footer and page css improvement

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Navbar() {
   return (
     <header className="text-gray-600 shadow-sm">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
         <Link href='/' className="text-xl font-medium text-gray-900 mb-4 md:mb-0">Digitalmish</Link>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
           <Link href='/' className="mr-5 hover:text-gray-900 cursor-pointer">Explore</Link>
